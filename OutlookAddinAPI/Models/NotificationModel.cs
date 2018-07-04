73bc73f4217c34af6dc3302206ea1659cefab0d9
JonathanJonathanJ <Jonathan@JONATHAN-PC>
2018-07-04 08:09:16 -0600

    Removed old notification classes

--- OutlookAddinAPI/Models/NotificationModel.cs
+++ OutlookAddinAPI/Models/NotificationModel.cs
@@ -1,9 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using Newtonsoft.Json;
 
-namespace OutlookAddinAPI.Models
+namespace OutlookNotificationsAPI.Models
 {
     public class NotificationModel
     {
--- OutlookAddinAPI/Models/ResourceDataModel.cs
+++ OutlookAddinAPI/Models/ResourceDataModel.cs
@@ -1,9 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
-
-namespace OutlookAddinAPI.Models
+﻿namespace OutlookNotificationsAPI.Models
 {
     public class ResourceDataModel
     {
